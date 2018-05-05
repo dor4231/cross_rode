de085c49f2109831df397baaedb43b1c0dac7311
Dor Bar <dor4231@gmail.com>
2018-05-05 23:09:54 +0300

    feat: 7 rows to the board

--- js/engine.js
+++ js/engine.js
@@ -25,7 +25,7 @@ var Engine = (function(global) {
         lastTime;
 
     canvas.width = 808;
-    canvas.height = 606;
+    canvas.height = 707;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
@@ -112,9 +112,10 @@ var Engine = (function(global) {
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
+                'images/grass-block.png',    // Row 2 of 2 of grass
                 'images/grass-block.png'    // Row 2 of 2 of grass
             ],
-            numRows = 6,
+            numRows = 7,
             numCols = 8,
             row, col;
         
@@ -173,6 +174,9 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
+        'images/enemy-pink-ghost.png',
+        'images/enemy-ninja-ghost.png',
+        'images/enemy-pirate-ghost.png',
         'images/char-boy.png'
     ]);
     Resources.onReady(init);
